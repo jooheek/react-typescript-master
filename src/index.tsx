04ce783cd2d4203e07e7cc8f047a67442e61f61b
jooheek <kimjoohee713@gmail.com>
2022-02-10 21:59:18 +0900

    Create and mend coin detail page, import react-query, applied react-query in mainpage

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { ThemeProvider } from "styled-components";
-import { theme } from "./theme";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
+const queryClient = new QueryClient();
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { fetchCoins } from '../api';
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -50,7 +52,7 @@ const Img = styled.img`
   margin-right: 10px;
 `;
 
-interface CoinInterface {
+interface ICoin {
   id: string;
   name: string;
   symbol: string;
@@ -61,7 +63,8 @@ interface CoinInterface {
 }
 
 function Coins() {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const { isLoading, data } = useQuery<ICoin[]>('allCoins', fetchCoins);
+  /*   const [coins, setCoins] = useState<CoinInterface[]>([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     //상세페이지에 들어갔다가 나오면 또다시 coin api를 불러온다.
@@ -69,23 +72,23 @@ function Coins() {
     //state로 behind the scene 소통을 한다.
     //https://v5.reactrouter.com/web/api/Link
     (async () => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const response = await fetch('https://api.coinpaprika.com/v1/coins');
       const json = await response.json();
       //console.log(json);
       setCoins(json.slice(0, 100));
       setLoading(false);
     })();
-  }, []);
+  }, []); */
   return (
     <Container>
       <Header>
         <Title>Coin</Title>
       </Header>
       <CoinList>
-        {loading ? (
+        {isLoading ? (
           <Loader>loading...</Loader>
         ) : (
-          coins.map((coin) => (
+          data?.slice(0, 100).map((coin) => (
             <Coin key={coin.id}>
               <Link
                 to={{
